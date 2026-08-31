7df5bf160e2aeefa0f98663c23db1418e87eafdb
Ali Ammari <ammari.ali.0001@gmail.com>
2026-08-31 22:54:00 +0100

    upgrade to v1.2.16

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
 
   // App Info
   static const String appName = 'SportConnect';
-  static const String appVersion = '1.2.15';
+  static const String appVersion = '1.2.16';
 
   // Firebase Collections
   static const String usersCollection = 'users';
